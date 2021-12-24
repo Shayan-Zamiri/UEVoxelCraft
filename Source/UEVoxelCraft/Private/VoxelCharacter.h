9e7c0b5cb2e19947cb45ee2dcc007d4f50164639
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2021-12-24 17:24:14 +0330

    Character-Basic movement
    Basic movement and controls

---
+++ Source/UEVoxelCraft/Private/VoxelCharacter.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "VoxelCharacter.generated.h"
+
+class UCameraComponent;
+
+UCLASS()
+class AVoxelCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	// CTOR/DTOR & VIRTUAL FUNCTIONS
+public:
+	// Sets default values for this character's properties
+	AVoxelCharacter();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// FUNCTIONS
+private:
+	UFUNCTION()
+	void MoveForward(float InVal);
+
+	UFUNCTION()
+	void MoveRight(float InVal);
+
+	// PROPERTIES
+private:
+	UPROPERTY()
+	UCameraComponent* PlayerCamera;
+};
