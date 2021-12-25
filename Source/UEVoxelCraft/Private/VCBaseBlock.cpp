c975083e5fbdfef1745de3934775b9c07ddab5f5
Shayan <shayan.zamiri@gmail.com>
2021-12-25 17:43:01 +0330

    Implemented BaseBlock

---
+++ Source/UEVoxelCraft/Private/VCBaseBlock.cpp
@@ -0,0 +1,56 @@
+﻿#include "VCBaseBlock.h"
+#include "Components/BoxComponent.h"
+
+// STATICS
+
+float AVCBaseBlock::BlockSize = 100.0f;
+
+// CTOR/DTOR & VIRTUAL FUNCTIONS
+
+AVCBaseBlock::AVCBaseBlock() : bIsBreakable{true}
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootSceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootSceneComp"));
+#if WITH_EDITOR
+	RootSceneComp->bVisualizeComponent = true;
+#endif
+	RootComponent = RootSceneComp;
+
+	SMComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SMComp"));
+	SMComp->SetSimulatePhysics(false);
+	SMComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	SMComp->SetupAttachment(RootSceneComp);
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	BoxComp->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
+	BoxComp->SetCollisionObjectType(ECC_WorldStatic);
+	BoxComp->SetBoxExtent(FVector{BlockSize/2, BlockSize/2, BlockSize/2});
+	BoxComp->SetupAttachment(SMComp);
+
+	SMComp->SetRelativeLocation(FVector{BlockSize/2, BlockSize/2, BlockSize/2});
+}
+
+void AVCBaseBlock::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	GridSnapBlock();
+}
+
+// FUNCTIONS
+
+FVector AVCBaseBlock::GetCenterOfCube() const
+{
+	return RootSceneComp->GetComponentLocation() +
+		(GetActorRightVector() + GetActorUpVector() + GetActorForwardVector()) * BlockSize / 2;
+}
+
+FVector AVCBaseBlock::GetAttachLocation(const FVector& ClickedLocation, const FVector& Normal)
+{
+	return (ClickedLocation + Normal * BlockSize / 2).GridSnap(BlockSize);
+}
+
+void AVCBaseBlock::GridSnapBlock()
+{
+	SetActorLocation(GetActorLocation().GridSnap(BlockSize));
+}
---
+++ Source/UEVoxelCraft/Public/VCBaseBlock.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "VCBaseBlock.generated.h"
+
+class UBoxComponent;
+
+UCLASS(Abstract, Blueprintable, BlueprintType)
+class UEVOXELCRAFT_API AVCBaseBlock : public AActor
+{
+
+	GENERATED_BODY()
+	
+	// CTOR/DTOR & VIRTUAL FUNCTIONS
+public:
+	AVCBaseBlock();
+	virtual ~AVCBaseBlock() override = default;
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	// FUNCTIONS
+public:
+	FVector GetCenterOfCube() const;
+
+protected:
+	FVector GetAttachLocation(const FVector& ClickedLocation, const FVector& Normal);
+	void GridSnapBlock();
+
+	// GETTERS & SETTERS
+public:
+	// PROPERTIES
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category= "Components")
+	USceneComponent* RootSceneComp;
+
+	// doesn't have collision
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category= "Components")
+	UStaticMeshComponent* SMComp;
+
+	// for collision data
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category= "Components")
+	UBoxComponent* BoxComp;
+
+	static float BlockSize; // ex:100 100 100
+
+protected:
+	float bIsBreakable; // TODO: Health Component
+};
