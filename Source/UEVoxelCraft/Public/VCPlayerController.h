ddf187c2cdf2dc2f4f5aa780ca30ea3bd44054f5
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2021-12-28 11:42:03 +0330

    CheatManager-Force garbage collection

---
+++ Source/UEVoxelCraft/Public/VCPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "VCPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEVOXELCRAFT_API AVCPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
