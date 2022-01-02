4b2a9e1848bcd6d28a1525ec6c0ffe26dabf2f0b
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2022-01-02 17:24:37 +0330

    Created InventoryComponent class
    Also a small reformatting on VCItemDataAsset

---
+++ Source/UEVoxelCraft/Private/VCInventoryComponent.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VCInventoryComponent.h"
+
+// CTOR/DTOR & VIRTUAL FUNCTIONS 
+
+// Sets default values for this component's properties
+UVCInventoryComponent::UVCInventoryComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UVCInventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UVCInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+// FUNCTIONS
+
---
+++ Source/UEVoxelCraft/Public/VCInventoryComponent.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "VCInventoryComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class UEVOXELCRAFT_API UVCInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+	// CTOR/DTOR & VIRTUAL FUNCTIONS
+public:
+	// Sets default values for this component's properties
+	UVCInventoryComponent();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	// FUNCTIONS
+public:
+
+	// GETTERS & SETTERS
+public:
+
+	// PROPERTIES
+protected:
+	
+};
