f1a65a5d58b3106382c558ea03d9648d5cb9e45a
Shayan <shayan.zamiri@gmail.com>
2022-01-02 17:28:01 +0330

    Added FVCEditorItemSlot
    + small reformating in VCItemSlot

--- Source/UEVoxelCraft/Public/VCTypes.h
+++ Source/UEVoxelCraft/Public/VCTypes.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-
+#include "VCAssetManager.h"
 #include "VCTypes.generated.h"
 
 UENUM(BlueprintType)
@@ -16,8 +16,39 @@ USTRUCT(BlueprintType)
 struct UEVOXELCRAFT_API FVCBiomes
 {
 	GENERATED_BODY()
-	
+
 	EVCBiomeType BiomeType;
 	int8 Temperature;
-	uint8 Rainfall;   // 0 - 100
+	uint8 Rainfall; // 0 - 100
+};
+
+USTRUCT(BlueprintType)
+struct UEVOXELCRAFT_API FVCEditorItemSlot
+{
+	GENERATED_BODY()
+
+	FVCEditorItemSlot() : SlotNumber{0}, SlotMaxCount{0}, SlotItemCount{0}, SlotItemType(UVCAssetManager::InventoryItem)
+	{
+	}
+
+	UPROPERTY(EditAnywhere, Category= "Properties")
+	uint8 SlotNumber;
+
+	UPROPERTY(EditAnywhere, Category= "Properties")
+	uint8 SlotMaxCount;
+
+	UPROPERTY(EditAnywhere, Category= "Properties")
+	uint8 SlotItemCount;
+
+	UPROPERTY(EditAnywhere, Category= "Properties")
+	FPrimaryAssetType SlotItemType;
+
+
+	friend uint32 GetTypeHash(const FVCEditorItemSlot& key)
+	{
+		uint32 Hash = 0;
+		Hash = HashCombine(Hash, key.SlotNumber);
+		Hash = HashCombine(Hash, GetTypeHash(key.SlotItemType));
+		return Hash;
+	}
 };
