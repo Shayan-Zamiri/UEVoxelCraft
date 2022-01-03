6c6ebd143effc5f9d53c356f14932373d4bb3e49
Shayan <shayan.zamiri@gmail.com>
2022-01-03 15:45:05 +0330

    Added basic properties to VCInventoryComponent

--- Source/UEVoxelCraft/Public/VCInventoryComponent.h
+++ Source/UEVoxelCraft/Public/VCInventoryComponent.h
@@ -3,9 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "VCTypes.h"
 #include "Components/ActorComponent.h"
 #include "VCInventoryComponent.generated.h"
 
+class UVCItemDataAsset;
+class UVCItemSlot;
 
 UCLASS(ClassGroup=(VoxelCraft), meta=(BlueprintSpawnableComponent))
 class UEVOXELCRAFT_API UVCInventoryComponent : public UActorComponent
@@ -28,6 +31,21 @@ public:
 public:
 
 	// PROPERTIES
-protected:
-	
+private:
+	UPROPERTY(EditAnywhere, Category= "Properties|Inventory")
+	TMap<FVCEditorItemSlot, UVCItemDataAsset*> InventorySlotsEditor;
+
+	UPROPERTY()
+	TArray<UVCItemDataAsset*> ItemsStrongReferences;
+
+	UPROPERTY()
+	TArray<UVCItemSlot*> ItemSlotsStrongReferences;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Properties|Inventory")
+	UVCItemSlot* EquippedSlot;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Properties|Inventory")
+	TMap<UVCItemDataAsset*, uint8 /**count*/> InventoryData;
+
+	TSortedMap<uint8, TPair<UVCItemSlot*, UVCItemDataAsset*>> InventorySlots;
 };
