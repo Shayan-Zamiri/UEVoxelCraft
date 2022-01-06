e4d082790f401c6f9811a46dc980fbc800d684be
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2022-01-06 10:39:55 +0330

    Simple crosshair
    Implemented the functionality for a simple crosshair to be drawn

---
+++ Source/UEVoxelCraft/Private/VCHUD.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VCHUD.h"
+
+#include "Engine/Canvas.h"
+
+void AVCHUD::DrawHUD()
+{
+	Super::DrawHUD();
+
+	// Didn't use a check() so you can play the game without a crosshair
+	if (CrosshairTex->IsValidLowLevel())
+	{
+		FVector2D Center{Canvas->ClipX / 2, Canvas->ClipY / 2};
+
+		FCanvasTileItem TileItem{Center, CrosshairTex->Resource, FLinearColor::Red};
+		TileItem.BlendMode = SE_BLEND_Translucent;
+
+		Canvas->DrawItem(TileItem);
+	}
+}
---
+++ Source/UEVoxelCraft/Public/VCHUD.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "VCHUD.generated.h"
+
+UCLASS(Blueprintable)
+class UEVOXELCRAFT_API AVCHUD : public AHUD
+{
+	GENERATED_BODY()
+
+	// CTOR/DTOR & VIRTUAL FUNCTIONS
+public:
+	AVCHUD() = default;
+	virtual ~AVCHUD() override = default;
+
+	virtual void DrawHUD() override;
+
+	// PROPERTIES
+private:
+	UPROPERTY(EditAnywhere, Category= "Properties")
+	UTexture2D* CrosshairTex;
+};
