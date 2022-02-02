4cdbe652e79b7cfa1e020942c4aabd97a8f67dde
Shayan <shayan.zamiri@gmail.com>
2022-02-02 14:45:37 +0330

    Added and Implemented VCItemSlotUI

---
+++ Source/UEVoxelCraft/Private/VCItemSlotUI.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VCItemSlotUI.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
+
+// CTOR/DTOR & VIRTUAL FUNCTIONS
+
+UVCItemSlotUI::UVCItemSlotUI(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer), SlotNumber{nullptr}, ItemCount{nullptr}, ItemIcon{nullptr}
+{
+}
---
+++ Source/UEVoxelCraft/Public/VCItemSlotUI.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "VCItemSlotUI.generated.h"
+
+class UImage;
+class UTextBlock;
+
+UCLASS()
+class UEVOXELCRAFT_API UVCItemSlotUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+	// CTOR/DTOR & VIRTUAL FUNCTIONS
+public:
+	UVCItemSlotUI(const FObjectInitializer& ObjectInitializer);
+
+	// FUNCTIONS
+public:
+	// GETTERS & SETTERS
+public:
+	// PROPERTIES
+public:
+	UPROPERTY(BlueprintReadWrite, Category= "Properties", meta= (BindWidget))
+	UTextBlock* SlotNumber;
+	
+	UPROPERTY(BlueprintReadWrite, Category= "Properties", meta= (BindWidget))
+	UTextBlock* ItemCount;
+
+	UPROPERTY(BlueprintReadWrite, Category= "Properties", meta= (BindWidget))
+	UTextBlock* ItemDescription;
+
+	UPROPERTY(BlueprintReadWrite, Category= "Properties", meta= (BindWidget))
+	UImage* ItemIcon;
+};
